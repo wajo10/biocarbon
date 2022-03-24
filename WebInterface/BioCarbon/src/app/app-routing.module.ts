10ce5ee3e837160e2d64827b07ec651e4d2e7c2b
Wajib Zaglul <wajib10@gmail.com>
2022-03-24 00:01:49 -0600

    [add] charts

--- WebInterface/BioCarbon/src/app/app-routing.module.ts
+++ WebInterface/BioCarbon/src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {LoginComponent} from './login/login.component';
+import {ChartsComponent} from './charts/charts.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/Login', pathMatch: 'full' },
   {path: 'Login', component: LoginComponent},
-  {path: 'Dashboard' , component: DashboardComponent}
+  {path: 'Dashboard' , component: DashboardComponent},
+  {path: 'Charts', component: ChartsComponent}
 ];
 
 @NgModule({
