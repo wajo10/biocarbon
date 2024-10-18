263d5a2eb302413882b7809df8fd5480f52d9fd1
Anibal Ruiz Barquero <44439445+Anibalru@users.noreply.github.com>
2024-10-17 18:36:58 -0600

    Update queries.js
    Actualizando ecuación del sensor 5 de la caja J
    Prueba con el sensor 4 d la caja E

--- API Server/queries.js
+++ API Server/queries.js
@@ -347,10 +347,10 @@ function humidityEquation(humidity, box, sensor) {
                     return (3 * 10 ** 24 * humidity ** -5.243)
                 case "sensor3":
                     return (-52.93*Math.log(humidity) + 560.91)
-                    //return (humidity ** 2 * 4 * 10 ** -8 + humidity * -0.0039 + 98.049)
+                    return (humidity ** 2 * 4 * 10 ** -8 + humidity * -0.0039 + 98.049)
                 case "sensor4":
-                    return (-52.93*Math.log(humidity) + 560.91)
-                    //return (humidity ** 2 * 1 * 10 ** -7 + humidity * -0.0084 + 156.71)
+                    //return (-52.93*Math.log(humidity) + 560.91)
+                    return (humidity ** 2 * 1 * 10 ** -7 + humidity * -0.0084 + 156.71)
                 case "sensor5":
                     return (humidity ** 2 * 5 * 10 ** -8 + humidity * -0.0051 + 126.7)
             }
@@ -423,7 +423,8 @@ function humidityEquation(humidity, box, sensor) {
                 case "sensor4":
                     return (humidity ** 2 * 9 * 10 ** -9 + humidity * -0.0019 + 70.907)
                 case "sensor5":
-                    return (humidity ** 2 * 1 * 10 ** -8 + humidity * -0.0022 + 92.534)
+                    return (-52.93*Math.log(humidity) + 560.91)
+                    //return (humidity ** 2 * 1 * 10 ** -8 + humidity * -0.0022 + 92.534)
             }
             break;
         case "K":
